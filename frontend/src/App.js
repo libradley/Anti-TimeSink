f0434746d0d2b37f6a276188e712396294fe0ca6
Samuel Parkman <Parkmans@OregonState.edu>
2025-02-03 22:57:07 -0500

    Added other pages and navLink to move between

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { NavLink, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
+
+// Import the BlockWebpage component
+import BlockWebpage from './block_webpage';
+import CurrentlyBlocked from './currently_blocked';
+import HistoryPage from './history';
+import StatisticsPage from './statistics';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router> {/* Wrap the whole app with Router */}
+      <div className="App">
+        <header className="App-header">
+          <div className="grid-container">
+            <NavLink to="/block_webpage" className={({ isActive }) => isActive ? "grid-item block-webpage active" : "grid-item block-webpage"}>Block Webpage</NavLink>
+            <NavLink to="/history" className={({ isActive }) => isActive ? "grid-item history active" : "grid-item history"}>History</NavLink>
+            <NavLink to="/statistics" className={({ isActive }) => isActive ? "grid-item statistics active" : "grid-item statistics"}>Statistics</NavLink>
+            <NavLink to="/currently_blocked" className={({ isActive }) => isActive ? "grid-item currently-blocked active" : "grid-item currently-blocked"}>Currently Blocked</NavLink>
+          </div>
+        </header>
+
+        <main>
+          {/* Define Routes */}
+          <Routes>
+            <Route path="/currently_blocked" element={<CurrentlyBlocked />} />
+            <Route path="/block_webpage" element={<BlockWebpage />} />
+            <Route path="/history" element={<HistoryPage />} />
+            <Route path="/statistics" element={<StatisticsPage />} />
+            
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/currently_blocked.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const CurrentlyBlocked = () => {
+  return (
+    <div>
+      <h1>Currently Blocked Page</h1>
+    </div>
+  );
+}
+
+export default CurrentlyBlocked;
